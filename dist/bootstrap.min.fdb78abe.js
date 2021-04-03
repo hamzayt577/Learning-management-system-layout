545cb37be708ac5c1df6d7b1bd8c390f1bd9368a
hamzayt577 <hamzayt577@gmail.com>
2021-04-03 19:15:46 +0400

    added sidebar

--- dist/bootstrap.min.fdb78abe.js
+++ dist/bootstrap.min.fdb78abe.js
@@ -1177,7 +1177,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60280" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55664" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
